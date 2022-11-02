971dc264ad3561e427c442e62c3c4af5c0c7f12a
Shayan <ShayanSadeghi1996@gmail.com>
2022-11-02 19:41:36 +0330

    added video capture

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
+from pickle import TRUE
 import numpy as np
 import cv2 as cv
 
-img = cv.imread("/home/shayan/Pictures/faces/pexels-photo-3184419.jpeg")
-
-# cv.imshow("gray",gray)
 
 face_cascade = cv.CascadeClassifier("haarcascade_frontalface_alt.xml")
 
 
-def face_detect(frame):
+def face_blur(frame):
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.05, 1)
 
     for (x, y, w, h) in faces:
-        # cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        roi = frame[y:y + int(1.5 * h), x:x + int(1.5 * w)]
-        blurred_face = cv.GaussianBlur(roi, (23, 23), 30)
-        frame[y:y + int(1.5 * h), x:x + int(1.5 * w)] = blurred_face
+        
+        if y + int(1.5 * h) < frame.shape[0]:
+            h = int(1.5 * h)
+
+        if x + int(1.5 * w) < frame.shape[1]:
+            w = int(1.5 * w)
+
+        cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        roi = frame[y : y + h, x : x + w]
+        blurred_face = cv.GaussianBlur(roi, (23, 23), 25)
+        frame[y : y + h, x : x + w] = blurred_face
     return frame
 
 
-blurred_img = face_detect(img)
-cv.imshow("blurred", blurred_img)
+capture = cv.VideoCapture(0)
 
-cv.imshow("face", img)
+while True:
+    ret,frame = capture.read()
+    frame = face_blur(frame)
+    cv.imshow("Video",frame)
 
-cv.waitKey(0)
+    if cv.waitKey(1) == ord('q'):
+        break
+
+# blurred_img = face_blur(img)
+# cv.imshow("blurred", blurred_img)
+# cv.waitKey(0)
