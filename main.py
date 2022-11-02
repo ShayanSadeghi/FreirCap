95a134fc6809ccb741e48c3dcaccd94fa6ab36a0
Shayan <ShayanSadeghi1996@gmail.com>
2022-11-02 19:58:14 +0330

    save video file

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def face_blur(frame):
     faces = face_cascade.detectMultiScale(gray, 1.05, 1)
 
     for (x, y, w, h) in faces:
-        
+
         if y + int(1.5 * h) < frame.shape[0]:
             h = int(1.5 * h)
 
@@ -26,14 +26,24 @@ def face_blur(frame):
 
 
 capture = cv.VideoCapture(0)
+fourcc = cv.VideoWriter_fourcc(*"XVID")
+video_size = (int(capture.get(3)), int(capture.get(4)))
+out = cv.VideoWriter("output.mp4", fourcc, 10.0, video_size)
 
 while True:
-    ret,frame = capture.read()
+    ret, frame = capture.read()
+    frame = cv.flip(frame, 1)
     frame = face_blur(frame)
-    cv.imshow("Video",frame)
+    out.write(frame)
 
-    if cv.waitKey(1) == ord('q'):
+    cv.imshow("Video", frame)
+
+    if cv.waitKey(1) == ord("q"):
         break
+
+capture.release()
+out.release()
+cv.destroyAllWindows()
 
 # blurred_img = face_blur(img)
 # cv.imshow("blurred", blurred_img)
